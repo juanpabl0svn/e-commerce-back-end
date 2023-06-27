98292418884ea17d33d8a90ad3a21a179e9ea835
Juan Pablo Sanchez <Juan Pablo Sanchez@DESKTOP-U4HB8MQ>
2023-06-27 17:20:04 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import * as url from "url";
+import express from "express";
+import cors from "cors";
+import path from "path";
+import morgan from "morgan";
+
+const __filename = url.fileURLToPath(import.meta.url);
+export const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname, "public","images"))
+})
+
+console.log(__dirname + "public\\images")
+
+app.listen(PORT, console.log(`Listening on port ${PORT}`));
